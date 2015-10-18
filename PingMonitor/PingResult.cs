234255a5e15bf734b233a07122b04d94392cfb9a
Kai Yang <yk000123@sina.com>
2015-10-18 22:13:22 +0800

    Rename PingTool to PingMonitor

---
+++ PingMonitor/PingResult.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+
+namespace Kfstorm.PingMonitor
+{
+    public class PingResult
+    {
+        public DateTime DateTime { get; set; }
+        public TimeSpan TimeCost { get; set; }
+        public IPStatus Status { get; set; }
+    }
+}
